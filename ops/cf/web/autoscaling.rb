6dca3daa5e70cca3d2451a24d6510b451af6fb27
Richard Lister <rlister@gmail.com>
2017-03-05 10:39:15 -0500

    allow scale down to zero to save money on test stacks

--- ops/cf/web/autoscaling.rb
+++ ops/cf/web/autoscaling.rb
@@ -2,8 +2,8 @@ resource :asg, 'AWS::AutoScaling::AutoScalingGroup' do
   VPCZoneIdentifier Fn::ref(:subnetids)
   launch_configuration_name Fn::ref(:lc)
   load_balancer_names [Fn::ref(:elb)]
-  min_size 1
-  max_size 2
+  min_size 0
+  max_size 3
   desired_capacity 1
   tag :Name, Fn::ref('AWS::StackName'), PropagateAtLaunch: true
 end
