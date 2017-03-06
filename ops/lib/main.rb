fd79599254f5ae48c0b3f3eeba235f2eac834f65
Richard Lister <rlister@gmail.com>
2017-03-06 15:28:07 -0500

    override docker registry

--- ops/lib/main.rb
+++ ops/lib/main.rb
@@ -10,6 +10,19 @@ module Stax
   add_stack :vpc
   add_stack :web
 
+  class Docker < Base
+
+    no_commands do
+      def registry
+        @_registry ||= '232121879002.dkr.ecr.us-east-1.amazonaws.com'
+      end
+
+      def repository
+        @_repository ||= "#{registry}/dumpcomstock"
+      end
+    end
+  end
+
   class Web < Stack
     include Asg, Elb, Ec2, Lmi, Keypair, Ssm
 
